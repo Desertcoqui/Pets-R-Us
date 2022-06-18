4841a03a9954d6ad38dd2cc72537f1fc5e89dde1
desertcoqui <fdetres@my365.bellevue.edu>
2022-06-18 18:49:55 -0500

    templates fixed

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+
 const app = express();
 const port = 3000;
 
@@ -8,11 +9,15 @@ app.use("/images", express.static(__dirname + "public/images"));
 app.use("/js", express.static(__dirname + "public/js"));
 app.use("/styles", express.static(__dirname + "public/img"));
 
-//HTML routes
+//HTML Routes
+app.engine(".html", require("ejs").__express);
+app.set("views", "./views");
+app.set("view engine", "html");
 
 app.get("", (req, res) => {
-  res.sendFile(__dirname + "/views/index.html");
+  res.render("index");
 });
+
 //Listening on port 3000
 
 app.listen(port, () => {
