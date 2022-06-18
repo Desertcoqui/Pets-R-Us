b3af8bc72939ce680da4e7fb7f053a97cf75910f
desertcoqui <fdetres@my365.bellevue.edu>
2022-06-18 18:23:24 -0500

    routes updted

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 const express = require("express");
 const app = express();
+const port = 3000;
 
-const PORT = process.env.Port || 3000;
+//Static Files Images, JS, CSS Styles
+app.use(express.static("public"));
+app.use("/images", express.static(__dirname + "public/images"));
+app.use("/js", express.static(__dirname + "public/js"));
+app.use("/styles", express.static(__dirname + "public/img"));
 
-app.get("/", (req, res) => {
-  res.send("We are up and running");
+//HTML routes
+
+app.get("", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
 });
+//Listening on port 3000
 
-app.listen(PORT, () => {
-  console.log("Application started and listening on port" + PORT);
+app.listen(port, () => {
+  console.log("Application started and listening on port" + port);
 });
-
-app.engine('.html',require('ejs')._express);
-
-app.set('views',path.join(/views/index.html,'views'));
-app.set('views',path.join(/views/grooming.html,'views'));
-
-app.set('view engine','html');
