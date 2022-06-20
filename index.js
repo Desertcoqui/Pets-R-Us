9ddfab6a691db145e93db1fa8188d93636180f0f
desertcoqui <fdetres@my365.bellevue.edu>
2022-06-19 21:24:40 -0500

    pets

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const port = 3000;
 app.use(express.static("public"));
 app.use("/images", express.static(__dirname + "public/images"));
 app.use("/js", express.static(__dirname + "public/js"));
-app.use("/styles", express.static(__dirname + "public/img"));
+app.use("/styles", express.static(__dirname + "public/styles"));
+app.use("/styles/site.css", express.static(__dirname + "public/styles/site.css"));
 
 //HTML Routes
 app.engine(".html", require("ejs").__express);
